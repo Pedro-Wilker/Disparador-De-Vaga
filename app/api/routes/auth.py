67c4fe2301abd92660e08cfce5a74efa2fb28436
Pedro Wilker <pedrowilker1516@gmail.com>
2026-06-09 21:41:19 -0300

    create profile and users

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -2,13 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.db.database import get_db
 import app.db.models as models
-import app.schemas.schemas as schemas
-from app.core.security import gerar_hash_senha
+from app.schemas.schemas import Usuario, UsuarioRegistro
+from app.core.security import gerar_hash_senha, verificar_senha, criar_token_acesso 
+from fastapi.security import OAuth2PasswordRequestForm 
 
 router = APIRouter(prefix="/api/auth", tags=["Autenticação"])
 
-@router.post("/registrar", response_model=schemas.Usuario, status_code=status.HTTP_201_CREATED)
-def registrar_usuario(usuario: schemas.UsuarioRegistro, db: Session = Depends(get_db)):
+@router.post("/registrar", response_model=Usuario, status_code=status.HTTP_201_CREATED)
+def registrar_usuario(usuario: UsuarioRegistro, db: Session = Depends(get_db)):
     
     usuario_existente = db.query(models.Usuario).filter(models.Usuario.email == usuario.email).first()
     if usuario_existente:
@@ -30,3 +31,18 @@ def registrar_usuario(usuario: schemas.UsuarioRegistro, db: Session = Depends(ge
     db.refresh(novo_usuario)
 
     return novo_usuario
+
+@router.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    usuario = db.query(models.Usuario).filter(models.Usuario.email == form_data.username).first()
+    
+    
+    if not usuario or not verificar_senha(form_data.password, usuario.senha_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="E-mail ou senha incorretos."
+        )
+    
+    token = criar_token_acesso({"sub": usuario.email})
+    
+    return {"access_token": token, "token_type": "bearer"}
--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,11 @@
+from datetime import datetime, timedelta
+from jose import jwt
 from passlib.context import CryptContext
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +14,9 @@ def gerar_hash_senha(senha: str) -> str:
 
 def verificar_senha(senha_plana: str, senha_hash: str) -> bool:
     return pwd_context.verify(senha_plana, senha_hash)
+
+def criar_token_acesso(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
